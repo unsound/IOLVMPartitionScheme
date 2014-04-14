1c0a7cb9a22e50a59136b72dd48e79c32c3b494c
Erik Larsson <catacombae@gmail.com>
2014-04-14 13:32:35 +0200

    lvm2_text.c: Bump copyright years.

--- libtlvm/lvm2_text.c
+++ libtlvm/lvm2_text.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (C) 2012 Erik Larsson
+ * Copyright (C) 2012-2014 Erik Larsson
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
